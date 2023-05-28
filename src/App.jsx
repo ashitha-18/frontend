cae40ac4ec266cadcf92dae6dd8d2f957ca70653
ashitha-18 <ashi18pulari@gmail.com>
2023-05-28 08:07:35 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
 import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Center, Square, Circle } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    Adhaar: '',
+    Pay: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform form submission logic or data processing here
+    console.log(formData);
+  };
+
 
   return (
     <>
-      <Header/>
+    <div>
+    <Center h='100px' color='black'>
+    <Heading>EQUIID</Heading>
+    </Center>
+    <Center h='100px' color='black'>
+
+    <h4 className='sub'>Are you ready for a groundbreaking solution that ensures secure identities and promotes fair pay for all? Look no further! EquiID is a revolutionary decentralized identity system built on the Polygon network, designed to empower individuals and foster equality in the digital era.</h4>
+    </Center>
+    </div>
+    <form onSubmit={handleSubmit}>
+    
+      <div>
+      <Center h='100px' color='black'>
+        <label htmlFor="Adhaar">Upload Adhaar Card :</label>
+        <input
+          type="file"
+          id="Adhaar"
+          name="Adhaar"
+          value={formData.Adhaar}
+          onChange={handleInputChange}
+        />
+        </Center>
+      </div>
+      <div>
+      <Center h='100px' color='black'>
+      <label htmlFor="Pay">Upload Pay Slip :</label>
+        <input
+          type="file"
+          id="Pay"
+          name="Pay"
+          value={formData.Pay}
+          onChange={handleInputChange}
+        />
+        </Center>
+      </div>
+      <div>
+      <Center h='100px' color='white'>
+      <Button colorScheme='blue'type="submit">Submit</Button>
+      </Center>
+      </div>
+    </form>
     </>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,18 @@
-function Header() {
-        
-            <div>
-                <a href="">Home</a>
-                <a href="">About Us</a>
-                <a href=""> How to Use</a>
-                <a href="">Get verified</a>
-            </div>
+import { useState } from 'react'
 
+function Header() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+        <a href="src\components\about\index.jsx">Home</a>
+        <a href="">About Us</a>
+        <a href=""> How to Use</a>
+        <a href="">Get verified</a>
+    </div>
+
+    )
+        
+           
 }
 
 export default Header
